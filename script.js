57e1452b2a7f39b8a8f93d2f9ad86aa71f36ae66
Lynch555 <vishneuski1983@mail.ru>
2022-07-04 14:06:23 +0300

    Lesson15hard

--- script.js
+++ script.js
@@ -1,25 +1,75 @@
-'use strict'
+const form = document.querySelector('.form');
+const headerInput = document.querySelector('.header-input');
+const body = document.querySelector('body');
 
-const DomElement = function () {
-    this.selector = '';
-    this.height = 100;
-    this.width = 100;
-    this.bg = 'red';
-    this.fontSize = 20;
-
-    this.getString = function () {
+const DomElement = function (selector, height, widt, bg, fontSize) {
+    this.selector = selector;
+    this.height = height;
+    this.width = widt;
+    this.bg = bg;
+    this.fontSize = fontSize;
+    this.createEl = function () {
         if (this.selector[0] == '.') {
-            document.body.innerHTML = '<div class="' + this.selector.slice(1) + '">Новый div</div>'
-            document.querySelector('div').style.cssText = 'height: ' + this.height + 'px; width: ' + this.width + 'px; background: ' + this.bg + '; font-size: ' + this.fontSize + 'px'
+            let newDiv = document.createElement('div');
+            newDiv.classList.add(`${this.selector}`.replace(/[^a-zа-яё]/gi, ''));
+            body.append(newDiv);
+            newDiv.style.cssText = ` 
+            height:${this.height};
+            width:${this.width};
+            background:${this.bg};
+            font-size:${this.fontSize};
+            position: absolute;`;
+            newDiv.textContent = `проверка`;
         } else if (this.selector[0] == '#') {
-            document.body.innerHTML = '<p id="' + this.selector.slice(1) + '">Новый параграф</p>'
-            document.querySelector('p').style.cssText = 'height:' + this.height + 'px; width:' + this.width + 'px; background: ' + this.bg + '; font-size: ' + this.fontSize + 'px'
+            let newDiv = document.createElement('div');
+            newDiv.id = `${this.selector.replace(/\s/g, '').replace(/[^a-zа-яё]/gi, '')}`;
+            body.append(newDiv);
+            newDiv.style.cssText = ` 
+            height:${this.height};
+            width:${this.width};
+            background:${this.bg};
+            font-size:${this.fontSize};
+            position: absolute;`;
+            newDiv.textContent = `проверка`;
         }
-    }
-}
+    };
+    this.createEl();
+    this.location = document.querySelector(`${this.selector}`);
+};
+const mover = function (currentElement) {
+    let currentRight = 0;
+    let currentLeft = 0;
+    let currentDown = 0;
+    let currentUp = 0;
+    currentElement.style.right = 0;
+    currentElement.style.left = 0;
+    currentElement.style.bottom = 0;
+    currentElement.style.top = 0;
+    document.onkeydown = function (e) {
+        currentElement.style.position = 'absolut';
+        if (e.key == 'ArrowRight') {
+            currentRight = currentRight + 10;
+            currentElement.style.left = `${currentRight}px`;
+        }
+        if (e.key == 'ArrowLeft') {
+            currentRight = currentRight - 10;
+            currentElement.style.left = `${currentRight}px`;
+        }
+        if (e.key == 'ArrowUp') {
+            currentUp = currentUp - 10;
+            currentElement.style.top = `${currentUp}px`;
+        }
+        if (e.key == 'ArrowDown') {
+            currentUp = currentUp + 10;
+            currentElement.style.top = `${currentUp}px`;
+        }
 
-const element = new DomElement();
-element.selector = '.div';
-element.getString();
 
-console.log(element);
+
+    };
+
+};
+
+const newDom1 = new DomElement('.block', '100px', '100px', 'red', '12px');
+
+mover(newDom1.location);
